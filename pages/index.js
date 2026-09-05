9d0faa565ae37d47b3c4e72a4b96033f9d0cf0e1
SohaibAhmadu <78642775+Sohaibahmadu@users.noreply.github.com>
2026-09-05 14:52:41 +0500

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
-      <h1>YouTube to Spotify Sync</h1>
-      <p>Project is live and ready for API connection.</p>
-    </main>
-  );
-}
-
