4cb48b048aab3a56ecde777ac92386e9a5529968
SohaibAhmadu <78642775+Sohaibahmadu@users.noreply.github.com>
2026-09-07 22:30:31 +0500

    Update spotify.js

--- pages/api/auth/spotify.js
+++ pages/api/auth/spotify.js
@@ -2,7 +2,6 @@ export default function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 
-  // Liked songs save karne ke liye 'user-library-modify' scope ka hona lazmi hai
   const scopes = [
     'playlist-read-private',
     'playlist-modify-public',
@@ -13,7 +12,7 @@ export default function handler(req, res) {
 
   const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(
     scopes
-  )}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  )}&show_dialog=true&redirect_uri=${encodeURIComponent(redirect_uri)}`;
 
   res.redirect(authUrl);
 }
