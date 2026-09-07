e5f652f00e4f9b87d6a6d431ba3379261c1c2569
SohaibAhmadu <78642775+Sohaibahmadu@users.noreply.github.com>
2026-09-07 21:20:57 +0500

    Update Spotify redirect URI and scopes

--- pages/api/auth/spotify.js
+++ pages/api/auth/spotify.js
@@ -1,15 +1,18 @@
 export default function handler(req, res) {
   const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const redirect_uri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/spotify`;
-  
+  const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+
   const scopes = [
     'playlist-read-private',
     'playlist-modify-public',
-    'playlist-modify-private'
+    'playlist-modify-private',
+    'user-library-read',
+    'user-library-modify'
   ].join(' ');
 
-  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(
+    scopes
+  )}&redirect_uri=${encodeURIComponent(redirect_uri)}`;
 
   res.redirect(authUrl);
 }
-
